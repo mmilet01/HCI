3307a85b3a5f4e77b6d4db379496037984f8aa4a
Karlo Mileta <mmilet01@fesb.hr>
2019-02-16 22:13:45 +0100

    cache removed

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('D:/Users/Karlo/Desktop/FAKS/SET_SAIL/HCI/node_modules/gatsby-remark-images/gatsby-browser'),
-      options: {"plugins":[],"maxWidth":1000,"linkImagesToOriginal":false,"backgroundColor":"transparent"},
-    },{
-      plugin: require('D:/Users/Karlo/Desktop/FAKS/SET_SAIL/HCI/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
